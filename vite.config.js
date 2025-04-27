94cf064e0f5f506b13dfc9fe6f931d38698f5eec
Anatoly <anatolii.kozmin@inbox.ru>
2025-04-27 12:37:52 +0300

    Add new dir

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default {
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles/variables.scss";`, // Глобальные переменные
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Удобный alias для импорта
+    },
+  },
+};
